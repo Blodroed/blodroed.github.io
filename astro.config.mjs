b20bac884ebf34433a2e061622d1b43998534b8e
Blodroed <epicwaterz0@gmail.com>
2025-09-26 16:21:28 +0200

    ✨ add @astrojs/node adapter and set server side rendering mode, prerender for static pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blodroed.com',
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: 'server',
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
